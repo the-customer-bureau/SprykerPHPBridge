85bc38e78ac9932b80076e7c8db84293f8974dee
Ray Lawlor <webcraftniray@users.noreply.github.com>
2022-10-19 23:54:04 +0100

    Added shared dependency to Resource

--- src/Resource/ResourceDependencyProvider.php
+++ src/Resource/ResourceDependencyProvider.php
@@ -4,12 +4,25 @@ declare(strict_types=1);
 
 namespace Engineered\Resource;
 
+use Engineered\Shared\SharedFacade;
 use Gacela\Framework\AbstractDependencyProvider;
 use Gacela\Framework\Container\Container;
 
 final class ResourceDependencyProvider extends AbstractDependencyProvider
 {
-    public function provideModuleDependencies(Container $container): void
-    {
-    }
+	public const FACADE_SHARED = 'FACADE_SHARED';
+
+	public function provideModuleDependencies(Container $container): void
+	{
+		$this->addSharedFacade($container);
+	}
+
+	private function addSharedFacade(Container $container) {
+		$container->set(
+			self::FACADE_SHARED,
+			function (Container $container) {
+				return $container->getLocator()->get(SharedFacade::class);
+			}
+		);
+	}
 }
