6ac62f44c3256417233a5578c5d7f791b8b48a9a
Ray Lawlor <webcraftniray@users.noreply.github.com>
2022-10-19 23:49:16 +0100

    Concrete Product Boilerplate

---
+++ src/Resource/ResourceFacade.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Engineered\Resource;
+
+use Gacela\Framework\AbstractFacade;
+
+/**
+ * @method ResourceFactory getFactory()
+ */
+final class ResourceFacade extends AbstractFacade
+{
+}
---
+++ src/Resource/ResourceFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Engineered\Resource;
+
+use Gacela\Framework\AbstractFactory;
+
+/**
+ * @method ResourceConfig getConfig()
+ */
+final class ResourceFactory extends AbstractFactory
+{
+}
